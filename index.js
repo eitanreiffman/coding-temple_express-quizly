425d91b046f0c3612556a6b188342f7152767724
Eitan Reiffman <eitanreiffman@gmail.com>
2023-01-30 15:20:23 -0500

    Added EJS, Render, and Navbar

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs')
+
 app.get('/', (request, response) => {
-    response.send('Hello World!')
+    response.render('register', {username: "Eitan"});
 });
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
+
+// EJS - Embedded JavaScript Templates
